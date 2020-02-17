11891b73bacbf57ef0cbbf4c7b9c782e803a254e
Will Axtell <waxtell@transtar1.com>
2020-02-17 16:51:32 -0500

    Converted from gist to real boy

---
+++ Host.ActivateService.Extension/IHostExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.Hosting
+{
+    public static class IHostExtensions
+    {
+        public static IHost ActivateService<TService>(this IHost host)
+        {
+            host.Services.GetService<TService>();
+
+            return host;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Host.ActivateService.Extension
 Force service activation post HostBuilder build.
+
+```csharp
+ public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args)
+                .Build()
+                .ActivateService<YourTGoesHere>()
+                .StartAsync()
+            ;
+        }
+```
